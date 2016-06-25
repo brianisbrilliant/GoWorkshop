e2ddc2852a2804e60828834e72cdad56c418b400
Brian Foster <brianisbrilliant@gmail.com>
2016-06-25 10:17:31 -0500

    Now i can print text!

--- greet/main.go
+++ greet/main.go
@@ -1,2 +1,15 @@
 // hello world
 // June 25th, 2016
+/* go is a compiled language.
+	every go file has to be in a package
+	package.main
+*/
+
+package main
+
+import "fmt" // for printing stuff out
+
+// accepts noparenthesis
+func main() {
+	fmt.Println("Hello world!")
+}
