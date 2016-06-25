21f84a4e8978d20f97f6e3f8556007e8d209334e
Brian Foster <brianisbrilliant@gmail.com>
2016-06-25 10:52:39 -0500

    errors on variables that aren't used.

---
+++ variables/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var myName string = "Brian"
+	fmt.Println("My name is " + myName + ".")
+
+	myName = "Voldemort"
+	fmt.Println("Just kidding my name is " + myName + "!!!")
+
+	// don't have to say what the variable type is
+	// this is scary but i need to learn it.
+	var myFavNum, aNum = 13, 400
+	fmt.Println(aNum, myFavNum)
+
+	var name string = "Butters"
+	nae := "Butters"
+}
