fca579bc54a6d354406c081300ea619a3baab880
Brian Foster <brianisbrilliant@gmail.com>
2016-06-25 10:36:17 -0500

    Go formats stuff for you!

--- greet/main.go
+++ greet/main.go
@@ -1,15 +1,31 @@
-// hello world
+// Brian Foster
+// hello world!
 // June 25th, 2016
-/* go is a compiled language.
-	every go file has to be in a package
-	package.main
+
+/*
+	to run this program, open the greet folder in the terminal and type "go install"
+	then type the name of the folder your project is in, for example "greet" and your project will run.
+	"go build" will create an exe!
 */
 
+// 	every go file has to be in a package
 package main
 
-import "fmt" // for printing stuff out
+import "fmt" // for printing stuff out to the console
 
-// accepts noparenthesis
+type User struct {
+	name  string
+	email string
+	age   int
+	badge int
+}
+
+// accepts no parenthesis
 func main() {
+	// i don't wanna type this all the time!
 	fmt.Println("Hello world!")
+	fmt.Println("How do I turn autocomplete off?!!?!?!?")
+	fmt.Println("Two commands at once!") // type one ampersand inbetween your commands. Ex. "go instsall & greet"
+	// go is built for efficiency, not for academics. there is one way to do things. it's like C# in that it's explicitly styled.
+	// gofunct automatically removes white space and other stuff that's not necessary.
 }
